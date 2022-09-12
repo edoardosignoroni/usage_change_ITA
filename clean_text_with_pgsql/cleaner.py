ffb613d0f455ef516dffccefd02491cbfd899971
DavideStabelli <davidestabelli@gmail.com>
2022-09-12 23:51:01 +0200

    added lemmatizer

--- clean_text_with_pgsql/cleaner.py
+++ clean_text_with_pgsql/cleaner.py
@@ -1,6 +1,7 @@
 import pg_manager as pgm
 import raw_parser as raw
 import cleaner_utils as ut
+from simplemma import lemmatize
 import os
 
 def generate_clean_file(out_path, file_name):
@@ -23,6 +24,35 @@ def generate_clean_file(out_path, file_name):
     with open(out_path, 'w+', encoding='utf-8') as out_file:
         for line in result:
             out_file.write(line[0] + '\n')
+
+def generate_clean_file_lemmatized(out_path, file_name):
+    config = ut.config(section='cleaner')
+    query = f'''select string_agg(a.word, ' ')
+    from (
+        select dspf.word word, dspt.word_line word_line
+        from {file_name}_text dspt 
+        inner join {file_name}_dict dspf on dspf.word_code = dspt.word_code
+        where length(dspf.word) >= {config['min_word_len']}
+        and dspf.freq >= {config['min_freq']} 
+        and dspf.word ~ {config['regex_filter']}
+        order by word_line, word_position asc
+    ) as a
+    group by a.word_line
+    '''
+    result = pgm.pg_run_query(query, True)
+    print('Query done! Now Joining...')
+    print(len(result))
+    with open(out_path, 'w+', encoding='utf-8') as out_file:
+        i=0
+        ut.progress_bar(i, len(result))
+        for line in result:
+            splitted_line = line[0].split()
+            lemmatized_splitted_line = []
+            for word in splitted_line:
+                lemmatized_splitted_line.append(lemmatize(word,  lang=('it', 'en')))
+            out_file.write(' '.join(lemmatized_splitted_line) + '\n')            
+            ut.progress_bar(i, len(result))
+            i += 1
 
 def run_all(raw_dir_path, out_path, file_name):
     if not os.path.isdir(out_path):
@@ -64,3 +94,4 @@ def run_generate_clean_file(out_file, table_prefix):
 #run_raw_processing('E:\\Download\\data\\days_2019\\raw', 'E:\\test_cleaner', 'days_2019')
 #run_create_dataset('E:\\test_cleaner\\days_2019\\days_2019_temp.txt', 'days_2019')
 #run_generate_clean_file('E:\\test_cleaner\\days_2019\\days_2019_clean.txt', 'days_2019')
+generate_clean_file_lemmatized('E:\\test_cleaner\\days_2019\\days_2019_lemm.txt', 'days_2019')
