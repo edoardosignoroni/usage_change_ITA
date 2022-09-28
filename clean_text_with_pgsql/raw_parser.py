90b8c26771a523f3146db09002e69b9c5ee58086
DavideStabelli <davidestabelli@gmail.com>
2022-09-28 21:45:05 +0200

    added line and token counter on raw cleaner/joiner

--- clean_text_with_pgsql/raw_parser.py
+++ clean_text_with_pgsql/raw_parser.py
@@ -16,7 +16,8 @@ def process_raw(in_file, out_path):
         os.mkdir(out_path)
 
     filter_regex_url = re.compile("https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)")
-    filter_regex_tag = re.compile('\[.*\]')
+    #filter_regex_tag = re.compile('\[.*\]')
+    filter_regex_tag = re.compile('\[deleted\]|\[removed\]')
     filter_regex_quote = re.compile('^> .*$')    
     filter_regex_newline = re.compile('\n')
     filter_regex_separator = re.compile('[\(\)\{\}\[\]?!.,;:\'"“^”«»~’`®\<\>\+*_]')
@@ -28,10 +29,19 @@ def process_raw(in_file, out_path):
             stopword = filter_regex_newline.sub('', stopword)
             stopword_array.append(stopword.upper())
 
+    count_row_lines = 0
+    count_row_words = 0
+    count_clean_lines = 0
+    count_clean_words = 0
+
     with open(in_file, 'r', encoding="utf8") as in_file:        
         data = in_file.readlines()
+        count_row_lines = len(data)
         with open(save_path, 'w+', encoding='utf8') as fp:
             for line in data:
+                raw_splitted_line = line.split()
+                count_row_words += len(raw_splitted_line)
+
                 line = filter_regex_quote.sub('', line)
                 line = filter_regex_url.sub('', line)
                 line = filter_regex_tag.sub('', line)
@@ -42,7 +52,11 @@ def process_raw(in_file, out_path):
                     splitted_line[0] = splitted_line[0].lower()
                 splitted_line = [i for i in splitted_line if i.upper() not in stopword_array]
                 if len(splitted_line) >= int(config['min_word_in_line']):                
+                    count_clean_lines += 1
+                    count_clean_words += len(splitted_line)
                     fp.write(' '.join(splitted_line) + '\n')
+    
+    return [count_row_lines, count_row_words, count_clean_lines, count_clean_words]
 
 def join_files(in_dir, out_path, file_name):
     files = [str(x) for x in Path(f'{in_dir}').glob('**/*.txt')]
@@ -59,8 +73,16 @@ def join_files(in_dir, out_path, file_name):
 def process_all_raw(in_path, out_path):
     files = [str(x) for x in Path(f'{in_path}').glob('**/*.txt')]
 
+    size_counter = [0,0,0,0]
+
     for file in tqdm(files):
-        process_raw(file, out_path)
+        file_count = process_raw(file, out_path)
+        size_counter[0] += file_count[0]
+        size_counter[1] += file_count[1]
+        size_counter[2] += file_count[2]
+        size_counter[3] += file_count[3]
+    
+    print(f'RAW: {size_counter[0]} lines and {size_counter[1]} words | CLEAN: {size_counter[2]} lines and {size_counter[3]} words')
 
 def process_and_join(in_path, out_path, file_name):
     print('Cleaning all the raw files!')
